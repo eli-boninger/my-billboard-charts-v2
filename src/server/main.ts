6666ffb86f0acac9aa3810dc90cd8e6604c16fe2
Eli Boninger <eboninger@gmail.com>
2023-11-02 17:12:31 -0400

    set up mongoose connection

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,23 @@
+import "dotenv/config";
 import express from "express";
 import ViteExpress from "vite-express";
+import mongoose from "mongoose"
+import { ServerApiVersion } from "mongodb";
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
+mongoose.connect(process.env.ATLAS_URI || "", {serverApi: {
+  version: ServerApiVersion.v1,
+  strict: true,
+  deprecationErrors: true,
+}}).then(() => console.log("connected to mongo db")).catch((e) => console.error(e))
 
 const app = express();
+app.use(express.json());
 
-app.get("/hello", (_, res) => {
-  res.send("Hello Vite + React + TypeScript!");
-});
-
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+ViteExpress.listen(app, PORT, () =>
+  console.log(`Server is listening on port ${PORT}...`)
 );
+
+
+
